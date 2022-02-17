29e8445f06cdfd589b4c8373a722d8954fb1f294
jbecker44 <jbecker@project44.com>
2022-02-17 15:39:09 -0600

    add functions to init db

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 import click
-from flask import current_app, g
+from flask import Flask, current_app, g
 from flask.cli import with_appcontext
 
 
@@ -34,3 +34,8 @@ def init_db():
 def init_db_command():
     init_db()
     click.echo("Initialized the database.")
+
+
+def init_app(app: Flask):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
